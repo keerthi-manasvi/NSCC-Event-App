ff8cc706950b77554d97fd6850bbba3b57110cd8
keerthi-manasvi <manjumanasvi.k@gmail.com>
2025-09-12 18:45:21 +0530

    Update urls.py

--- NSCC/urls.py
+++ NSCC/urls.py
@@ -21,9 +21,9 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('Event.urls')),  # your app urls
+    path('', include('Event.urls')),  # Include your app urls
 ]
 
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+# Serve media files in both debug and production (temporary solution for Render)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
