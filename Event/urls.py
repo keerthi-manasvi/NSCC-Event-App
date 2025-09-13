d2e8d59ce3d71074a252f4396079d866bffac6cf
keerthi-manasvi <manjumanasvi.k@gmail.com>
2025-09-13 14:07:37 +0530

    Update urls.py

--- Event/urls.py
+++ Event/urls.py
@@ -1,21 +1,15 @@
-from django.conf import settings
-from django.conf.urls.static import static
-
 from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.register, name='register'),
-    path('qr/<str:reg_id>/', views.participant_qr, name='participant_qr'),
-    path('scan/', views.scan_page, name='scan'),
-    path('export_excel/', views.export_xlsx, name='export_excel'),
-    path('dashboard/', views.dashboard, name='dashboard'),
-    path('mark_attendance/<str:reg_id>/', views.mark_attendance, name='mark_attendance'),
-    path('clear/', views.clear_participants, name='clear_participants'),
-    path('signup/', views.SignUp_view, name='SignUp'),
-    path('login/', views.login_view, name='login'),
-    path('logout/', views.logout_view, name='logout'),
+    path("", views.register, name="register"),
+    path("qr/<str:reg_id>/", views.participant_qr, name="participant_qr"),
+    path("scan/", views.scan_page, name="scan"),
+    path("export_excel/", views.export_xlsx, name="export_excel"),
+    path("dashboard/", views.dashboard, name="dashboard"),
+    path("mark_attendance/<str:reg_id>/", views.mark_attendance, name="mark_attendance"),
+    path("clear/", views.clear_participants, name="clear_participants"),
+    path("signup/", views.SignUp_view, name="SignUp"),
+    path("login/", views.login_view, name="login"),
+    path("logout/", views.logout_view, name="logout"),
 ]
-
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
